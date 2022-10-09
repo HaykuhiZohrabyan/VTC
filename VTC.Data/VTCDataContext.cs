006f6812a7cf0dc57d9e6f6cb72a38097b1a1fac
Sergey S <catus88@outlook.com>
2022-10-09 14:59:04 +0400

    fix context

--- VTC.Data/VTCDataContext.cs
+++ VTC.Data/VTCDataContext.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using VTC.Data.Entities;
 namespace VTC.Data
 {
-    //public class VTCDataContext:IdentityDbContext<User>
-    //{
+    public class VTCDataContext : IdentityDbContext<User, IdentityRole<int>, int, IdentityUserClaim<int>, IdentityUserRole<int>, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
+    {
 
-    //}
+    }
 }
