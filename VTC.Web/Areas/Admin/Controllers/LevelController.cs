0f1d71a1979d828db41906ac44a3799078336209
Davit Karapetyan <davit207karapetyan@gmail.com>
2022-10-19 21:18:09 +0400

    Fix Lvel Controller

--- VTC.Web/Areas/Admin/Controllers/LevelController.cs
+++ VTC.Web/Areas/Admin/Controllers/LevelController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using VTC.Application.ViewModels;
 using VTC.Application.Services.Interfaces;
-using VTC.Application.ViewModels;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.Linq.Expressions;
 
@@ -10,7 +9,7 @@ namespace VTC.Web.Areas.Admin.Controllers
     [Area("Admin")]
     public class LevelController : Controller
     {
-        private readonly ILevelService _levelService ;
+        private readonly ILevelService _levelService;
         private readonly IPackageService _packageService;
         public LevelController(ILevelService levelService, IPackageService packageService)
         {
@@ -24,11 +23,39 @@ namespace VTC.Web.Areas.Admin.Controllers
             var data = _levelService.GetAllLevels(packageId);
             return View(data);
         }
-        public IActionResult AddEdit(int? levelId)
+        [HttpGet]
+        public IActionResult AddEdit(int? levelId, int? packageId)
         {
-            Leve
-            return PartialView();
+            var packages = _packageService.GetAllPackages();
+            ViewBag.Packages = _packageService.GetAllPackages();
+            LevelAddEditVM model = levelId.HasValue ? _levelService.GetForEdit(levelId.Value) : new();
+            if (packageId.HasValue)
+            {
+                model.PackageId = packageId.Value;
+                ViewBag.PackageSelectDisabled = true;
+                ViewBag.SelectedPackageName = packages.FirstOrDefault(p => p.Id == packageId.Value).Title;
+            }
+            return PartialView("_AddEdit", model);
+        }
+        [HttpPost]
+        public IActionResult AddEdit(LevelAddEditVM model)
+        {
+            if (model.Id == 0)
+            {
+                _levelService.Add(model);
+            }
+            else
+            {
+                _levelService.Update(model);
+            }
+            return RedirectToAction("Index");
+        }
+        public IActionResult Detail(LevelInfoVM model)
+        {
+            var detail = _levelService.GetInfoById(model.Id);
+            return View(detail);
+
         }
 
-    }      
+    }
 }
