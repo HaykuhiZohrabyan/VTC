7375f2071049b597442be764e7bf90f6f4473173
Haykuhi Zohrabyan <haykuhiz@mail.ru>
2022-11-04 20:06:42 +0400

    addlessonbutton

---
+++ VTC.Data/Repositories/Interfaces/ILessonRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VTC.Data.Entities;
+
+namespace VTC.Data.Repositories.Interfaces
+{
+    public interface ILessonRepository
+    {
+        void Add(Lesson model);
+        void Update(Lesson  model);  
+    }
+}
---
+++ VTC.Data/Repositories/LessonRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using VTC.Data.Entities;
+using VTC.Data.Repositories.Interfaces;
+
+namespace VTC.Data.Repositories
+{
+    public class LessonRepository : ILessonRepository
+    {
+        private readonly VTCDataContext _context;
+        public LessonRepository(VTCDataContext context)
+        {
+            _context=context;   
+        }
+        public void Add(Lesson model)
+        {
+           _context.Lessons.Add(model);
+        }
+
+        public void Update(Lesson model)
+        {
+            var data=_context.Lessons.FirstOrDefault(l=>l.Id == model.Id); 
+            
+        }
+    }
+}
