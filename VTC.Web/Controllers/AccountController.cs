61a0c026a81d672b09754791a58367d20303561a
Haykuhi Zohrabyan <haykuhiz@mail.ru>
2023-01-18 19:31:25 +0400

    LoginForm

--- VTC.Web/Controllers/AccountController.cs
+++ VTC.Web/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Http.Extensions;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using VTC.Application.ViewModels;
+using VTC.Application.ViewModels.User;
 using VTC.Data.Entities;
 
 namespace VTC.Web.Controllers
@@ -15,6 +17,14 @@ namespace VTC.Web.Controllers
             _userManager = userManager;
             _signInManager = signInManager;
         }
+        public IActionResult Index()
+        {
+
+            String pageUrl = "";
+            pageUrl = Request.Query["index"];
+            ViewBag.url=pageUrl;
+            return View();
+        }
         [HttpGet]
         public IActionResult Register()
         {
@@ -23,6 +33,7 @@ namespace VTC.Web.Controllers
         [HttpPost]
         public async Task<IActionResult> Register(UserRegisterVM model)
         {
+            
             if (ModelState.IsValid)
             {
                 User user = new User { Email = model.Email, UserName = model.Email,
@@ -46,6 +57,51 @@ namespace VTC.Web.Controllers
                 }
             }
             return View(model);
+        }
+        [HttpGet]
+        public IActionResult Login(string returnUrl = null)
+        {
+
+            
+
+            return View(new LoginViewModel {ReturnUrl=returnUrl });
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        { 
+            if (ModelState.IsValid)
+            {
+                var result =
+                    await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+                if (result.Succeeded)
+                {
+                    if (!string.IsNullOrEmpty(model.ReturnUrl) && Url.IsLocalUrl(model.ReturnUrl))
+                    {
+                        return RedirectToAction("Index","Account");
+                       
+                    }
+                    else
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Неправильный логин и (или) пароль");
+                }
+            }
+            return View(model);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
